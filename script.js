10af5c1b16b78a074f0cc57e999ffbf0246cf8d4
wacoss <52801670+wacoss@users.noreply.github.com>
2025-07-14 01:13:03 -0400

    Update script.js

--- script.js
+++ script.js
@@ -26,7 +26,7 @@
             //'Telefe': 'canales/telefenoticias.html',  
             //'Radio Bio Bio': 'canales/radiobiobio.html',   
             //'Latina Perú': 'canales/latina.html',  
-            'Caracol Colombia': 'canales/caracol.html',  
+            'Caracol': 'canales/caracol.html',  
             'CGTN': 'canales/cgtn.html',  
             '90 Hits': 'canales/90hits.html',   
             'Bob esponja': 'canales/bobesponja.html', 
