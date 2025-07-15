03c700efad20c7a15ef226a631c0c9a22563be37
wacoss <52801670+wacoss@users.noreply.github.com>
2025-07-15 19:38:28 -0400

    Update script.js

--- telechileytelecampo/script.js
+++ telechileytelecampo/script.js
@@ -10,6 +10,16 @@
                   
             'Inicio': 'flipflapclock.html',  // si lo quitas el primer canal se reproducirá pausado,
             'TVN': 'miplayer.html?file=https://mdstrm.com/live-stream-playlist-v/5346f5f2c1e6f5810b5b9df0.m3u8',  
+            'TV+': 'miplayer.html?file=https://jireh-6-hls-video-cl-movistar.dps.live/hls-video/ey6283je82983je9823je8jowowiekldk9838274/tvmas/tvmas.smil/playlist.m3u8',   
+            'Mega': 'miplayer.html?file=https://unlimited1-cl-isp.dps.live/mega/mega.smil/playlist.m3u8',  
+            '13': 'miplayer.html?file=https://unlimited1-cl-isp.dps.live/t13radio/t13radio.smil/playlist.m3u8', 
+            'CHV==': 'canales/chv/index.html',    
+            'Canal 9': 'miplayer.html?file=https://unlimited6-cl.dps.live/c9/c9.smil/playlist.m3u8',   
+            'TVU==': 'canales/tvu/tvu.html', 
+            'UCV==': 'miplayer.html?file=https://unlimited2-cl-isp.dps.live/ucvtv2/ucvtv2.smil/ucvtv2/livestream2/chunks.m3u8',   
+
+              
+
             'CHV': 'canales/chv.html',   
             'TeleTrece': 'canales/teletrece.html',
             '24 Horas': 'canales/24horas.html',    
