10556df1edd395a36fbeb4ff043069b1175331de
wacoss <52801670+wacoss@users.noreply.github.com>
2025-07-16 18:42:14 -0400

    Update script.js

--- telechileytelecampo/script.js
+++ telechileytelecampo/script.js
@@ -19,7 +19,7 @@
             'UCV': 'miplayer.html?file=https://unlimited2-cl-isp.dps.live/ucvtv2/ucvtv2.smil/ucvtv2/livestream2/chunks.m3u8',   
             'Tevex': 'miplayer.html?file=https://v2.tustreaming.cl/tevex/index.m3u8',   
             'Tevex YT': 'canales/tevexyt.html', 
-            'Zona Latina': 'canales/zonalatina.html', 
+            'Zona Latina YT': 'canales/zonalatina.html', 
             'Teletrece': 'canales/teletrece.html',   
             '24 Horas': 'canales/24horas.html',   
             'Meganoticias': 'canales/meganoticias.html',   
