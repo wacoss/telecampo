a06dcc92cbfadbbd6c27f1b0f0033fe8a474ca11
wacoss <52801670+wacoss@users.noreply.github.com>
2025-07-15 20:01:45 -0400

    Update script.js

--- telechileytelecampo/script.js
+++ telechileytelecampo/script.js
@@ -12,11 +12,25 @@
             'TVN': 'miplayer.html?file=https://mdstrm.com/live-stream-playlist-v/5346f5f2c1e6f5810b5b9df0.m3u8',  
             'TV+': 'miplayer.html?file=https://jireh-6-hls-video-cl-movistar.dps.live/hls-video/ey6283je82983je9823je8jowowiekldk9838274/tvmas/tvmas.smil/playlist.m3u8',   
             'Mega': 'miplayer.html?file=https://unlimited1-cl-isp.dps.live/mega/mega.smil/playlist.m3u8',  
-            '13': 'miplayer.html?file=https://unlimited1-cl-isp.dps.live/t13radio/t13radio.smil/playlist.m3u8', 
+            ' 13': 'miplayer.html?file=https://unlimited1-cl-isp.dps.live/t13radio/t13radio.smil/playlist.m3u8', 
             'CHV': 'canales/chv.html',    
             'Canal 9': 'miplayer.html?file=https://unlimited6-cl.dps.live/c9/c9.smil/playlist.m3u8',   
-            'TVU**': 'canales/tvu.html', 
+            'TVU==': 'canales/tvu.html', 
             'UCV': 'miplayer.html?file=https://unlimited2-cl-isp.dps.live/ucvtv2/ucvtv2.smil/ucvtv2/livestream2/chunks.m3u8',   
+            'Tevex': 'miplayer.html?file=https://v2.tustreaming.cl/tevex/index.m3u8',   
+            'Tevex YT==': 'canales/tevexyt.html', 
+            'Zona Latina==': 'canales/zonalatina.html', 
+            'Teletrece': 'miplayer.html?file=https://jireh-3-hls-video-cl-movistar.dps.live/hls-video/10b92cafdf3646cbc1e727f3dc76863621a327fd/t13/t13.smil/playlist.m3u8',   
+            '24 Horas': 'canales/24horas.html',    
+            'CHV Noticias': 'canales/chvnoticias.html',
+            'DW': 'miplayer.html?file=https://dwamdstream104.akamaized.net/hls/live/2015530/dwstream104/stream01/streamPlaylist.m3u8',
+            'Euronews': 'https://www.youtube.com/embed/live_stream?channel=UCyoGb3SMlTlB8CLGVH4c8Rw&autoplay=true&mute=0',
+            'CBS News': 'miplayer.html?file=https://dai.google.com/linear/hls/event/EezGs5EpSjSr-sYjB2qysw/master.m3u8',
+            
+    
+
+
+              
             };
 
             // Función para mostrar los canales
