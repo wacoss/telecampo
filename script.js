9b841fae73fceb95e38a64316f28629c91383472
wacoss <52801670+wacoss@users.noreply.github.com>
2025-11-14 18:39:21 -0300

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@
             // Lista de canales
             const channels = {
                   
-            'InicioMEO': 'flipflapclock.html',  // si lo quitas el primer canal se reproducirá pausado,
+            'Inicio': 'flipflapclock.html',  // si lo quitas el primer canal se reproducirá pausado,
             'TVN': 'canales/tvn',  
             'CHV': 'canales/chv',   
             'TeleTrece*': 'canales/teletrece', // fallando en samsung
